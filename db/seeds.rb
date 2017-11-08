cb82d58b307b3005d4477d1e0e45fc9cd5832d19
Robin Zuschke <robin.l.zuschke@gmail.com>
2017-11-08 16:32:54 +0100

    Raked green and the backend works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.delete_all
+
+food_category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  Restaurant.create(name: Faker::Company.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone, category: food_category.sample)
+end
